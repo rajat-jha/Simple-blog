4cd82a54f38eeb93b923da0af3706f69e0620d93
xtremeCodeRJ <jharajat2k14@gmail.com>
2016-04-25 11:02:23 +0530

    extract redundancy

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ActiveRecord::Base
+	validates :apellation, presence:true, length: {minimum:4, maximum: 50}
+    validates :caption, presence:true, length: {minimum:10, maximum: 100}
+	validates :specification, presence:true, length: {minimum:4, maximum: 50}
 end
